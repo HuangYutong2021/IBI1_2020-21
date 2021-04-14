0323fdc1d61e5a0f825c28648da3a35f974cba6f
HuangYutong2021 <yutongh.20@intl.zju.edu.cn>
2021-04-14 15:01:10 +0800

    this uses functions and it can help teachers

---
+++ Practical10/Grade_calculator_function.py
@@ -0,0 +1,19 @@
+def helper(name, portfolio, poster, final):
+    Name=name
+    grade= float(portfolio)*0.4 + float(poster)*0.3 + float(final)*0.3
+    return (name,grade)
+
+print('please input your name')
+a=input()
+print('please input the portfolio grade of the student')
+b=input()
+print('please input the poster presentation grade of the student')
+c=input()
+print('please input the final exam grade of the student')
+d=input()
+ultimate=str(helper(a,b,c,d))
+ultimate=ultimate.replace("'", '')
+ultimate=ultimate.replace("(", '')
+ultimate=ultimate.replace(")", '')
+ultimate=ultimate.replace(",", '')
+print(ultimate)
