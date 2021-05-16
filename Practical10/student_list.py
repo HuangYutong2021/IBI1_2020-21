0895adc77fd95ecf7d90b74adb05ec5fa873d03a
HuangYutong2021 <yutongh.20@intl.zju.edu.cn>
2021-05-16 21:54:28 +0800

    add a print method within the class as expected

--- Practical10/student_list.py
+++ Practical10/student_list.py
@@ -3,7 +3,9 @@ class Student (object):
          self.first_name = first_name
          self.family_name = family_name
          self.programme = programme
-         print(first_name,family_name, programme)
+    def printing(self):
+        print(self.first_name, self.family_name, self.programme)
+
 
 print('please input the first name of the student')
 a=input()
@@ -12,3 +14,4 @@ b=input()
 print('please input the undergraduate programme of the student')
 c=input()
 i=Student(a,b,c)
+i.printing()
