7c84ca050b90368e8209447cd6fc38ea617d1ba2
HuangYutong2021 <yutongh.20@intl.zju.edu.cn>
2021-05-19 11:16:01 +0800

    add some pseudocodes

--- Practical10/Reverse_complement_calculator.py
+++ Practical10/Reverse_complement_calculator.py
@@ -2,12 +2,14 @@
 def transformation(i):
  code = {'A': 'T', 'C': 'G', 'T': 'A', 'G': 'C'}
  re = str(i)
+#Convert all letters to uppercase
  record = re.upper()
  se = ''
+#convert it to its complementary form
  for a in range(0,len(i)):
   one = record[a:a+1]
   se += str(code[one])
-
+#reverse its order
  for a in range(0,len(i)):
   print(se[len(i)-1-a:len(i)-a],end='')
 
