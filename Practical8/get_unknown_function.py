82176504f747c0767c9a7ffa4751bd1554df29d3
HuangYutong2021 <yutongh.20@intl.zju.edu.cn>
2021-05-19 11:19:39 +0800

    add some pseudocodes, too

--- Practical8/get_unknown_function.py
+++ Practical8/get_unknown_function.py
@@ -2,17 +2,19 @@ import re
 xfile= open('Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa','r')
 
 infor={}
+#store the data in the dictionary
 for line in xfile:
  if line.startswith('>'):
   key = str(line)
   infor[key]=''
  else:
   infor[key] = infor[key]+line.replace('\n','')
-
+#create a new file
 important = open('unknown_function.fa','w')
 for key in infor:
     if re.search('unknown',key):
         name = re.findall(r'gene:(\S+)\s', key)
+#remove the square brackets to make the results more clear
         key1 = str(name).replace("['", '')
         key2 = key1.replace("']", '')
         #the codes which start with "#" are those which I write to attain some goals mentioned during the pratical but need not to be included in my portfolio.
